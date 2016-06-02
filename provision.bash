beb2a7828e622724d51f16856ee6166d2a160090
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-06-02 16:47:54 -0500

    Create example netcdf files

--- provision.bash
+++ provision.bash
@@ -16,6 +16,31 @@ rpmquery --quiet netcdf.x86_64 \
 
 for env in dev test ops ; do
   exdir=/data/${env}/example
+  cdl=${exdir}/example_${env}.cdl
+  nc=${exdir}/example_${env}.nc
 
   mkdir -p -- "$exdir"
+  if ! [ -f "$cdl" ] ; then
+    cat > "$cdl" <<EOF
+netcdf example_$env {   // example of CDL notation
+dimensions:
+  x = 3 ;
+  y = 8 ;
+variables:
+  float rh(x, y) ;
+  rh:units = "percent" ;
+  rh:long_name = "relative humidity" ;
+// global attributes
+  :title = "example in $env" ;
+data:
+  rh =
+    2, 3, 5, 7, 11, 13, 17, 19,
+    23, 29, 31, 37, 41, 43, 47, 53,
+    59, 61, 67, 71, 73, 79, 83, 89 ;
+}
+EOF
+  fi
+  if [ "$cdl" -nt "$nc" ] ; then
+    ncgen -o "$nc" "$cdl"
+  fi
 done
