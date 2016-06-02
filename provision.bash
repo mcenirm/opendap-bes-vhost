fc8e6bd5904284e1f981f9a5e02d30369469f63e
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-06-02 16:45:27 -0500

    Install packages for hyrax bes

--- provision.bash
+++ provision.bash
@@ -4,3 +4,9 @@ set -e
 
 rpmquery --quiet epel-release \
 || yum -y install epel-release
+
+rpmquery --quiet libdap.x86_64 \
+|| yum -y install /vagrant/hyrax_releases/binary/hyrax-1.13.0/centos6.6/libdap{,-devel}-3.17.0-1.el6.x86_64.rpm
+
+rpmquery --quiet bes.x86_64 \
+|| yum -y install /vagrant/hyrax_releases/binary/hyrax-1.13.0/centos6.6/bes{,-devel}-3.17.0-1.static.el6.x86_64.rpm
