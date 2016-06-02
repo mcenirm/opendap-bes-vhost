987d33a52111d1d596b907143e6536878383be4f
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-06-02 16:46:27 -0500

    Install netcdf-related packages

--- provision.bash
+++ provision.bash
@@ -10,3 +10,6 @@ rpmquery --quiet libdap.x86_64 \
 
 rpmquery --quiet bes.x86_64 \
 || yum -y install /vagrant/hyrax_releases/binary/hyrax-1.13.0/centos6.6/bes{,-devel}-3.17.0-1.static.el6.x86_64.rpm
+
+rpmquery --quiet netcdf.x86_64 \
+|| yum -y install {netcdf,nco}{,-devel}.x86_64
